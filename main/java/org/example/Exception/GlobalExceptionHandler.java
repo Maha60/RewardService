b31f4217aa977134afe10b02e418aa7264fe20b6
B <mahalakshmi.b.b@capgemini.com>
2025-05-27 20:24:11 +0530

    Reward API Commited by maha

---
+++ main/java/org/example/Exception/GlobalExceptionHandler.java
@@ -0,0 +1,21 @@
+// exception/GlobalExceptionHandler.java
+package org.example.Exception;
+
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class GlobalExceptionHandler
+{
+
+     @ExceptionHandler(RewardsCalculationException.class)
+     public ResponseEntity<String> handleRewardsException(RewardsCalculationException ex)
+     {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.BAD_REQUEST);
+     }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleAll(Exception ex) {
+        return new ResponseEntity<>("Error: " + ex.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
---
+++ test/java/TestCases.java
@@ -0,0 +1,30 @@
+import org.example.Service.RewardsService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RewardServiceTest {
+
+
+    private RewardsService rewardService;
+
+
+
+    @Test
+    void testCalculatePoints() {
+        rewardService = new RewardsService();
+        // Directly testing the reward calculation logic
+        assertEquals(25, rewardService.CalculatePoints(75));  // 1x25
+        assertEquals(0, rewardService.CalculatePoints(50));   // no points
+        assertEquals(0, rewardService.CalculatePoints(30));   // no points
+        assertEquals(250, rewardService.CalculatePoints(200)); // 2x100 + 1x50
+      //  assertEquals("Transacion amount cannot be negative", rewardService.CalculatePoints(-120.00)); // 2x20 + 1x50
+      //negative test case scenario commented as of now
+    }
+
+
+
+
+}
+
